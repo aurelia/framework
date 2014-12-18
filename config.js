1e9819565f1e4fa024c38c1d08f05cc00757b96f
Rob Eisenberg <rob@bluespire.com>
2014-12-18 02:34:44 -0500

    fix(package): update templating to latest version

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ System.config({
     "aurelia-logging": "github:aurelia/logging@0.1.1",
     "aurelia-metadata": "github:aurelia/metadata@0.1.1",
     "aurelia-task-queue": "github:aurelia/task-queue@0.1.1",
-    "aurelia-templating": "github:aurelia/templating@0.3.0",
+    "aurelia-templating": "github:aurelia/templating@0.3.1",
     "github:aurelia/binding@0.1.2": {
       "aurelia-metadata": "github:aurelia/metadata@0.1.1",
       "aurelia-task-queue": "github:aurelia/task-queue@0.1.1"
@@ -28,14 +28,14 @@ System.config({
       "es6-shim": "github:paulmillr/es6-shim@0.21.1",
       "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.2"
     },
-    "github:aurelia/templating@0.3.0": {
+    "github:aurelia/templating@0.3.1": {
       "aurelia-binding": "github:aurelia/binding@0.1.2",
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.1.1",
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.1",
       "aurelia-loader": "github:aurelia/loader@0.2.0",
       "aurelia-logging": "github:aurelia/logging@0.1.1",
       "aurelia-metadata": "github:aurelia/metadata@0.1.1",
-      "aurelia-path": "github:aurelia/path@0.2.2",
+      "aurelia-path": "github:aurelia/path@0.3.0",
       "aurelia-task-queue": "github:aurelia/task-queue@0.1.1",
       "es6-shim": "github:paulmillr/es6-shim@0.21.1"
     }
