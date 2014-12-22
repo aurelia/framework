63d4c5f6db58a50f6e1776b2b4939ca81ce4edf5
Rob Eisenberg <rob@bluespire.com>
2014-12-22 01:38:20 -0500

    fix(package): update templating to latest version

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ System.config({
     "aurelia-logging": "github:aurelia/logging@0.1.1",
     "aurelia-metadata": "github:aurelia/metadata@0.1.1",
     "aurelia-task-queue": "github:aurelia/task-queue@0.1.1",
-    "aurelia-templating": "github:aurelia/templating@0.3.2",
+    "aurelia-templating": "github:aurelia/templating@0.4.0",
     "github:aurelia/binding@0.1.2": {
       "aurelia-metadata": "github:aurelia/metadata@0.1.1",
       "aurelia-task-queue": "github:aurelia/task-queue@0.1.1"
@@ -28,7 +28,7 @@ System.config({
       "es6-shim": "github:paulmillr/es6-shim@0.21.1",
       "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.2"
     },
-    "github:aurelia/templating@0.3.2": {
+    "github:aurelia/templating@0.4.0": {
       "aurelia-binding": "github:aurelia/binding@0.1.2",
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.1.1",
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.1",
