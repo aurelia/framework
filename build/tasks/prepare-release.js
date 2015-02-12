7696d417a0ee053f69976ae013b3cf41c4077566
Rob Eisenberg <rob@bluespire.com>
2015-02-12 09:13:33 -0500

    chore(build): re-organize requires

--- build/tasks/prepare-release.js
+++ build/tasks/prepare-release.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var paths = require('../paths');
-var args = require('../args');
 var changelog = require('conventional-changelog');
 var fs = require('fs');
 var bump = require('gulp-bump');
+var args = require('../args');
 
 gulp.task('bump-version', function(){
   return gulp.src(['./package.json', './bower.json'])
