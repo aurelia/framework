17cf46cdfbf4081e08c41db3c20a0590c885d254
Rob Eisenberg <rob@bluespire.com>
2015-05-27 14:44:28 +0200

    fix(index): ensure export of path module api

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ export * from 'aurelia-metadata';
 export * from 'aurelia-templating';
 export * from 'aurelia-loader';
 export * from 'aurelia-task-queue';
+export * from 'aurelia-path';
 
 import * as TheLogManager from 'aurelia-logging';
 export var LogManager = TheLogManager;
