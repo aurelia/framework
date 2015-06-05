9e91bcb24902d91b506dde720e33a8883009a314
Andreas Krummsdorf <a.krummsdorf@litixsoft.de>
2015-06-05 09:51:05 +0200

    chore: add task 'cover' to run tests with code coverage

--- build/tasks/test.js
+++ build/tasks/test.js
@@ -23,3 +23,24 @@ gulp.task('tdd', function (done) {
         done();
     });
 });
+
+/**
+ * Run test once with code coverage and exit
+ */
+gulp.task('cover', function (done) {
+  karma.start({
+    configFile: __dirname + '/../../karma.conf.js',
+    singleRun: true,
+    reporters: ['coverage'],
+    preprocessors: {
+      'test/**/*.js': ['babel'],
+      'src/**/*.js': ['babel', 'coverage']
+    },
+    coverageReporter: {
+      type: 'html',
+      dir: 'build/reports/coverage'
+    }
+  }, function (e) {
+    done();
+  });
+});
