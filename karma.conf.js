b9deaa9dc5a8a5addaa9d607d1b5a1679fbe1a07
bigopon <bigopon.777@gmail.com>
2022-03-28 23:43:17 +1100

    chore(larma): try run headless chrome

--- karma.conf.js
+++ karma.conf.js
@@ -71,7 +71,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
 
 
     // Continuous Integration mode
